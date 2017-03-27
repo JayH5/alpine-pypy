54c3dc785d0afd5a800dbac77ba654452428dfee
Jamie Hewland <jhewland@gmail.com>
2017-03-27 11:02:33 +0200

    PyPy3 packages should be called 'pypy3'

--- 3/build.sh
+++ 3/build.sh
@@ -4,7 +4,7 @@ set -ex
 BASE_DIR="/usr/src/pypy"
 PYTHON="$(which pypy || which python)"
 
-PYPY_NAME="pypy3.3"
+PYPY_NAME="pypy3"
 PYPY_RELEASE_VERSION="${PYPY_RELEASE_VERSION:-$PYPY_VERSION}"
 PYPY_ARCH="linux64"
 
